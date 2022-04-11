28d27fe96e8caff9efa14f56b8c07ec400e554e0
toan.kien@gmail.com <toan.kien@gmail.com>
2022-04-11 23:15:26 +1000

    Adding CORS

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express'); 
-
 const app = express();
-const bodyParser = require('body-parser');
 
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
+
+var cors = require('cors');
+app.use(cors());
 
 var shortid = require('shortid');
 
