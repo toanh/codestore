6e769b2a2912c4d33796ea0655988c6165e2b225
toan.kien@gmail.com <toan.kien@gmail.com>
2022-04-11 20:26:31 +1000

    Updated $PORT binding

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
-app.listen(3000, () => console.log('server started'));
+app.listen(process.env.PORT || 3000, () => console.log('server started'));
